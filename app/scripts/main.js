c8cddf7eca195014eb2866f1c739c6c710abdac6
Xiltyn <xiltyn@gmail.com>
2016-09-25 01:51:12 +0200

    skills.json + AJAX | skills % | 5 skills in a row fix | projects nav btns hover & active hotix | Footer logo repositioned

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -234,58 +234,7 @@ function showOverview() {
   );
 }
 
-function showSkills() {
-  var offsetElement    =  $('#scroll-skills');
-  var effectElement    =  $('.skills-container');
-  var effect           =  'animated fadeIn'
 
-  // get current distance from top of viewport
-  var currentTop = $(window).scrollTop();
-
-  $(window).scroll({
-    previousTop: 0
-    },
-
-  function() {
-    // define the header height here
-    var headerHeight = offsetElement.offset().top - 20;
-    // if user has scrolled past header, initiate the scroll up/scroll down hide show effect
-    if ($(window).scrollTop() > headerHeight) {
-      if (currentTop < this.previousTop) {
-      } else {
-      $(effectElement).addClass(effect);
-      loop(
-        function() {
-          var fillLevel
-
-          $('.el--bar').each(function(i) {
-            var $el = $(this);
-            var defaultLevel = 100;
-            var fillLevel  = +$(this).find('.el--bar-fill').attr('data-skillLevel');
-            var setTo = defaultLevel - fillLevel;
-
-            setTimeout(function() {
-              $el.find('.el--bar-fill').css('transform', 'translateY(' + setTo.toString() + '%)');
-            }, 250*i);
-          })
-        },
-        function() {
-          var $element = $('.el--bar-icn');
-
-          $element.each(function(i) {
-            var $el = $(this);
-            setTimeout(function() {
-              $el.addClass('el--bar-icn-active');
-              // console.log($(this));
-            }, 250*i);
-          });
-        });
-      }
-    }
-    this.previousTop = currentTop;
-    console.log(this.previousTop);
-  });
-}
 
 function toTopBtn() {
   var offsetElement    =  $('#overview');
@@ -310,222 +259,355 @@ function showQuote() {
   fadeMe('quote-fade', 'animated fadeInDown')
 }
 
-function showSkillName() {
-  var $el = $('.el--bar');
 
-  $el.each(function() {
-    var $icn = $(this).find('.el--bar-icn');
 
-    $(this).on('mouseover', function() {
-      $icn.addClass('el--bar-icn-active');
-      console.log('Mousing over ' + $icn);
-      // $(this).on('mouseleave', function() {
-      //   $icn.removeClass('el--bar-icn-active');
-      // })
-    })
-  })
-}
-
-function injectGallery() {
-// Function wide variables
 // ============================================================::||:>
-  var $projectElements;
-  var $projectBtns;
+// ======================= S K I L L S ========================::||:>
+// ============================================================::||:>
 
-  // Function parsing projects data into Underscore.js template
-  // ==========================================================::||:>
-  function renderBtns(data) {
-    var $template        = _.template($('#catTemp').html());
-    var $wrapper         = $('#navAjax');
-    var $objects         = Array.prototype.slice.call(data.projects)
-    var $fragment        = $(document.createDocumentFragment());
+  function injectSkills() {
 
-    var result = $objects.reduce(function(memo, e1){
-      var matches = memo.filter(function(e2){
-        return e1.category == e2.category
-      })
-      if (matches.length == 0)
-        memo.push(e1)
-        return memo;
-    }, [])
+    // Function parsing data into Underscore.js template
+    // ==========================================================::||:>
+    function render(data) {
+      var $template        = _.template($('#skillsTemp').html());
+      var $webdev          = $('#webdev');
+      var $design          = $('#design');
+      var $objects         = Array.prototype.slice.call(data.skills);
+      var $fragmentWebdev  = $(document.createDocumentFragment());
+      var $fragmentDesign  = $(document.createDocumentFragment());
 
-    result.forEach(function(element) {
-      $fragment.append($template({
-        category: element.category
-      }));
-    });
+      console.log($objects);
+      $objects.forEach(function(element) {
 
-    $wrapper.append($fragment);
-    $projectBtns = $('.category-btn');
-  };
+        if (element.category == "webdev") {
 
-  // Function parsing projects data into Underscore.js template
-  // ==========================================================::||:>
-  function render(data) {
-    var $template        = _.template($('#projTemp').html());
-    var $wrapper         = $('#galleryAjax');
-    var $objects         = data.projects;
-    var $fragment        = $(document.createDocumentFragment());
+          $fragmentWebdev.append($template({
+            id: element.id,
+            name: element.name,
+            cat: element.category,
+            level: element.level,
+            imgSrc: element.imgSrc
+          }));
 
-    $objects.forEach(function(element) {
-      $fragment.append($template({
-        id: element.id,
-        category: element.category,
-        name: element.name,
-        desc: element.desc,
-        repo: element.repo
-      }));
-    });
+        } else if (element.category == "design") {
 
-    $wrapper.append($fragment);
-    $projectElements = $('.project-el');
-  };
+          $fragmentDesign.append($template({
+            id: element.id,
+            name: element.name,
+            cat: element.category,
+            level: element.level,
+            imgSrc: element.imgSrc
+          }));
 
-  // Animate in thumbnails on scroll
-  // ==========================================================::||:>
-  function showProjects() {
-    var currentTop = $(window).scrollTop();
-    var offsetElement = $('#projects');
-    var elementGroupOne = $('.project-el');
-    var elementGroupTwo = $('.category-btn');
+        } else {
 
-    $(window).scroll( {previousTop: 0},
-      function() {
-        var offset = offsetElement.offset().top - 20;
+          console.log("sad element without a category screaaaams to You! @_@!!");
 
-        if ($(window).scrollTop() > offset) {
-          if (currentTop < this.previousTop) {
-          } else {
-            elementGroupOne.each(function(i) {
-              var element = $(this);
-              setTimeout(function() {
-                element.addClass('animated fadeIn');
-              }, 150*i);
-            });
-          }
         }
-        if ($(window).scrollTop() > offset) {
+      });
+
+      $webdev.append($fragmentWebdev);
+      $design.append($fragmentDesign);
+    };
+
+    function showSkills() {
+      var offsetElement    =  $('#scroll-skills');
+      var effectElement    =  $('.skills-container');
+      var effect           =  'animated fadeIn'
+
+      // get current distance from top of viewport
+      var currentTop = $(window).scrollTop();
+
+      $(window).scroll({
+        previousTop: 0
+        },
+
+      function() {
+        // define the header height here
+        var headerHeight = offsetElement.offset().top - 20;
+        // if user has scrolled past header, initiate the scroll up/scroll down hide show effect
+        if ($(window).scrollTop() > headerHeight) {
           if (currentTop < this.previousTop) {
           } else {
-            elementGroupTwo.each(function(i) {
-              var element = $(this);
-              setTimeout(function() {
-                element.addClass('animated fadeIn');
-              }, 150*i);
+          $(effectElement).addClass(effect);
+          loop(
+            function() {
+              var fillLevel
+
+              $('.el--bar').each(function(i) {
+                var $el = $(this);
+                var defaultLevel = 100;
+                var fillLevel  = +$(this).find('.el--bar-fill').attr('data-skillLevel');
+                var setTo = defaultLevel - fillLevel;
+
+                setTimeout(function() {
+                  $el.find('.el--bar-fill').css('transform', 'translateY(' + setTo.toString() + '%)');
+                }, 250*i);
+              })
+            },
+            function() {
+              var $element = $('.el--bar-icn');
+
+              $element.each(function(i) {
+                var $el = $(this);
+                setTimeout(function() {
+                  $el.addClass('el--bar-icn-active');
+                  // console.log($(this));
+                }, 250*i);
+              });
             });
           }
         }
         this.previousTop = currentTop;
-
-        // LOG CURRENT POSITION
-        // console.log(this.previousTop);
-      }
-    );
-  }
-
-  // Open lightbox on click
-  // ==========================================================::||:>
-  function showLightbox() {
-    var lightbox = $('.lightbox');
-    var lightboxTxt = $('.lightbox-wrapper--text');
-    var lightboxImg = $('.lightbox-wrapper--img');
-    var spinner = $('.spinner');
-
-    function hideLoader() {
-      spinner.css('opacity', '0');
+        console.log(this.previousTop);
+      });
     }
 
-    $projectElements.on('click', function(event) {
-      var $th       = $(this);
-      var $name     = $th.find('.project-el--thumb-overlay h3').text();
-      var $desc     = $th.find('.project-el--thumb-overlay p').text();
-      var $imgSrc   = $th.find('.project-el--thumb img').attr('data-bigSrc');
-      var $repo     = $th.find('.project-el--thumb img').attr('data-repo');
+    function showSkillName() {
+      var $el = $('.el--bar');
 
-      lightbox.addClass('animated fadeIn').css('pointer-events', 'all');
+      $el.each(function() {
+        var $icn = $(this).find('.el--bar-icn');
 
-      setTimeout(function() {
-        hideLoader();
+        $(this).on('mouseover', function() {
+          $icn.addClass('el--bar-icn-active');
+          console.log('Mousing over ' + $icn);
+          // $(this).on('mouseleave', function() {
+          //   $icn.removeClass('el--bar-icn-active');
+          // })
+        })
+      })
+    }
+
+    // AJAX deprecated call
+    // ==========================================================::||:>
+    $.when($.ajax("skills.json")).then(success, failure);
+
+    // Callback function called when objects are successfully loaded
+    // ==========================================================::||:>
+    function success(success) {
+      console.log("Yes! Success!");
+      render(success);
+      showSkills();
+      showSkillName();
+    }
+
+    // Callback function called when objects fail to be loaded
+    // ==========================================================::||:>
+    function failure() {
+      console.log('Whooops! Something went wrong with loading the JSON file data for S K I L L S!');
+    }
+  }
+
+// ============================================================::||:>
+// ====================== G A L L E R Y =======================::||:>
+// ============================================================::||:>
+
+  function injectGallery() {
+  // Function wide variables
+  // ============================================================::||:>
+    var $projectElements;
+    var $projectBtns;
+
+    // Function parsing projects data into Underscore.js template
+    // ==========================================================::||:>
+    function renderBtns(data) {
+      var $template        = _.template($('#catTemp').html());
+      var $wrapper         = $('#navAjax');
+      var $objects         = Array.prototype.slice.call(data.projects)
+      var $fragment        = $(document.createDocumentFragment());
+
+      var result = $objects.reduce(function(memo, e1){
+        var matches = memo.filter(function(e2){
+          return e1.category == e2.category
+        })
+        if (matches.length == 0)
+          memo.push(e1)
+          return memo;
+      }, [])
+
+      result.forEach(function(element) {
+        $fragment.append($template({
+          category: element.category
+        }));
+      });
+
+      $wrapper.append($fragment);
+      $projectBtns = $('.category-btn');
+    };
+
+    // Function parsing projects data into Underscore.js template
+    // ==========================================================::||:>
+    function render(data) {
+      var $template        = _.template($('#projTemp').html());
+      var $wrapper         = $('#galleryAjax');
+      var $objects         = data.projects;
+      var $fragment        = $(document.createDocumentFragment());
+
+      $objects.forEach(function(element) {
+        $fragment.append($template({
+          id: element.id,
+          category: element.category,
+          name: element.name,
+          desc: element.desc,
+          repo: element.repo
+        }));
+      });
+
+      $wrapper.append($fragment);
+      $projectElements = $('.project-el');
+    };
+
+    // Animate in thumbnails on scroll
+    // ==========================================================::||:>
+    function showProjects() {
+      var currentTop = $(window).scrollTop();
+      var offsetElement = $('#projects');
+      var elementGroupOne = $('.project-el');
+      var elementGroupTwo = $('.category-btn');
+
+      $(window).scroll( {previousTop: 0},
+        function() {
+          var offset = offsetElement.offset().top - 20;
+
+          if ($(window).scrollTop() > offset) {
+            if (currentTop < this.previousTop) {
+            } else {
+              elementGroupOne.each(function(i) {
+                var element = $(this);
+                setTimeout(function() {
+                  element.addClass('animated fadeIn');
+                }, 150*i);
+              });
+            }
+          }
+          if ($(window).scrollTop() > offset) {
+            if (currentTop < this.previousTop) {
+            } else {
+              elementGroupTwo.each(function(i) {
+                var element = $(this);
+                setTimeout(function() {
+                  element.addClass('inactive');
+                }, 150*i);
+              });
+            }
+          }
+          this.previousTop = currentTop;
+
+          // LOG CURRENT POSITION
+          // console.log(this.previousTop);
+        }
+      );
+    }
+
+    // Open lightbox on click
+    // ==========================================================::||:>
+    function showLightbox() {
+      var lightbox = $('.lightbox');
+      var lightboxTxt = $('.lightbox-wrapper--text');
+      var lightboxImg = $('.lightbox-wrapper--img');
+      var spinner = $('.spinner');
+
+      function hideLoader() {
+        spinner.css('opacity', '0');
+      }
+
+      $projectElements.on('click', function(event) {
+        var $th       = $(this);
+        var $name     = $th.find('.project-el--thumb-overlay h3').text();
+        var $desc     = $th.find('.project-el--thumb-overlay p').text();
+        var $imgSrc   = $th.find('.project-el--thumb img').attr('data-bigSrc');
+        var $repo     = $th.find('.project-el--thumb img').attr('data-repo');
+
+        lightbox.addClass('animated fadeIn').css('pointer-events', 'all');
 
         setTimeout(function() {
-          lightboxImg.html('<img src="' + $imgSrc + '" alt="' + $name + '" />');
-          lightboxTxt.html('<a href="' + $repo + '" target="_blank"><h3 class="animated flash">' + $name + '</h3></a><p>' + $desc + '</p>');
-        }, 300);
-      }, 1000);
+          hideLoader();
 
-      $('html').one('click', function() {
-        lightbox.removeClass('animated fadeIn').css('pointer-events', 'none');
-        lightboxImg.html('');
-        lightboxTxt.html('');
-        spinner.css('opacity', '1');
-        $('body').removeClass('stop-scrolling');
-      })
+          setTimeout(function() {
+            lightboxImg.html('<img src="' + $imgSrc + '" alt="' + $name + '" />');
+            lightboxTxt.html('<a href="' + $repo + '" target="_blank"><h3 class="animated flash">' + $name + '</h3></a><p>' + $desc + '</p>');
+          }, 300);
+        }, 1000);
 
-      // disable scrolling on body
-      // ======================================================::||:>
-      if ($('body').hasClass('stop-scrolling')) {
-
-      } else {
-        $('body').addClass('stop-scrolling');
-      }
-
-      event.stopPropagation();
-    })
-  }
-
-  function toggleCategory() {
-    var $btn = $('.category-btn');
-    var $allBtn = $('.all-btn');
-    var $el = $('.project-el');
-
-    $btn.on('click', function() {
-      var $btnCat = $(this).attr('data-category');
-      if ($(this).hasClass('all-btn')) {
-        $el.show();
-      } else {
-        $el.each(function() {
-          var $th = $(this);
-          var $elCat = $th.data('category');
-          if ($elCat == $btnCat) {
-            $th.show();
-          } else {
-            $th.hide();
-          }
+        $('html').one('click', function() {
+          lightbox.removeClass('animated fadeIn').css('pointer-events', 'none');
+          lightboxImg.html('');
+          lightboxTxt.html('');
+          spinner.css('opacity', '1');
+          $('body').removeClass('stop-scrolling');
         })
-      }
-    })
+
+        // disable scrolling on body
+        // ======================================================::||:>
+        if ($('body').hasClass('stop-scrolling')) {
+
+        } else {
+          $('body').addClass('stop-scrolling');
+        }
+
+        event.stopPropagation();
+      })
+    }
+
+    function toggleCategory() {
+      var $btn = $('.category-btn');
+      var $allBtn = $('.all-btn');
+      var $el = $('.project-el');
+
+      $btn.on('click', function() {
+        var $btnCat = $(this).attr('data-category');
+        if ($(this).hasClass('all-btn')) {
+          $el.show();
+        } else {
+          $el.each(function() {
+            var $th = $(this);
+            var $elCat = $th.data('category');
+            if ($elCat == $btnCat) {
+              $th.show();
+            } else {
+              $th.hide();
+            };
+          })
+        }
+        $btn.removeClass('active');
+        $(this).addClass('active');
+      })
+    }
+
+    // AJAX deprecated call
+    // ==========================================================::||:>
+    $.when($.ajax('projects.json')).then(success, failure);
+
+    // Callback function called when objects are successfully loaded
+    // ==========================================================::||:>
+    function success(success) {
+      render(success);
+      renderBtns(success);
+      showProjects();
+      showLightbox();
+      toggleCategory();
+    }
+
+    // Callback function called when objects fail to be loaded
+    // ==========================================================::||:>
+    function failure() {
+      console.log('Whooops! Something went wrong with loading the JSON file data for the Projects Gallery!');
+    }
+
   }
 
-  // AJAX deprecated call
-  // ==========================================================::||:>
-  $.when($.ajax('projects.json')).then(success, failure);
-
-  // Callback function called when objects are successfully loaded
-  // ==========================================================::||:>
-  function success(success) {
-    render(success);
-    renderBtns(success);
-    showProjects();
-    showLightbox();
-    toggleCategory();
-  }
-
-  // Callback function called when objects fail to be loaded
-  // ==========================================================::||:>
-  function failure() {
-    console.log('Whooops! Something went wrong with loading the JSON file data for the Projects Gallery!');
-  }
-
-}
-
-injectGallery();
-toggleMenu();
-showHeader();
-jbControl();
-scrollButtonClick();
-showSkills();
-scrollToElement();
-showOverview();
-toTopBtn();
+// Global callbacks
+// ==============================================================::||:>
+  injectSkills();
+  injectGallery();
+  toggleMenu();
+  showHeader();
+  jbControl();
+  scrollButtonClick();
+  scrollToElement();
+  showOverview();
+  toTopBtn();
 
 })(window.$, window._);
